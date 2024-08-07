e953bc0f3b3efaeba2c73196c6b565246a87cb27
codecraze25 <juanhub25@outlook.com>
2024-08-07 23:37:56 +0200

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+const App: React.FC = () => {
+  return <h1 className="text-center text-blue-500 text-lg">Hello World</h1>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the path according to your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
